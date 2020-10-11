977426d248fdc1db30f7b4e99b4557d146b7c0e6
sangwookim <sangwookim@sujileeui-MacBookPro.local>
2020-10-11 12:43:12 +0900

    Add 02-data-type-1.py int, float, string 타입에 대해서 실습함

---
+++ 02-data-type-1.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+# 정수 int - integer
+print(1 + 1)
+# 소수 float
+print (1.5 + 2.1)
+# 문자 str - string
+print ("hello")
+
+# 문자열 더하기
+print ("hello" + "1")
+
+# 정수소수간 변경
+print (float(4 / 2))
+
+# 기타테스트
+print (int("1"))
+print ("1")
+print (int("1.5"))
